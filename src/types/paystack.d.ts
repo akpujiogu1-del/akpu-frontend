9146eeddc10961a9df50989bb047b1073677d7ee
akpujiogu1-del <akpujiogu1@gmail.com>
2026-04-18 12:17:12 +0000

    fix: correct paystack type declaration syntax

--- src/types/paystack.d.ts
+++ src/types/paystack.d.ts
@@ -1,1 +1,27 @@
-declare module '@paystack/inline-js';
+declare module "@paystack/inline-js" {
+  interface TransactionSuccess {
+    reference: string;
+    trans: string;
+    status: string;
+    message: string;
+    transaction: string;
+    trxref: string;
+  }
+
+  interface PaystackOptions {
+    key: string;
+    email: string;
+    amount: number;
+    ref?: string;
+    currency?: string;
+    metadata?: Record<string, any>;
+    onSuccess: (transaction: TransactionSuccess) => void;
+    onCancel: () => void;
+  }
+
+  class PaystackPop {
+    newTransaction(options: PaystackOptions): void;
+  }
+
+  export default PaystackPop;
+}
